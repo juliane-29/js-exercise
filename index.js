bd935c41f82adcf715008640341844656b3d89ea
Juliane Huber <juliane.dominique.huber@gmail.com>
2020-03-08 10:10:42 +0100

    added some code

--- index.js
+++ index.js
@@ -1,103 +1,118 @@
 var rover = {
-    direction: "W",
-    // direction can be either North(N), East (E), South (S), West (W)
-    x: 0,
-    y: 0,
-    // current position on 10x10 array
-  };
-  //function when the rover turns left --> direction changes
-  function turnLeft(rover){
-    switch (rover.direction){
-      case "N":
-        rover.direction === "W";
-     //console.log("The rover is facing West");
-        break; 
-      case "W":
-        rover.direction === "S";
+  direction: "S",
+  // direction can be either North(N), East (E), South (S), West (W)
+  x: 0,
+  y: 0,
+  // current position on 10x10 array
+  travelLog: [{x:0,y:0}]
+};
+//function when the rover turns left --> direction changes
+function turnLeft(rover){
+  switch (rover.direction){
+    case "N":
+      rover.direction === "W";
+   //console.log("The rover is facing West");
+      break; 
+    case "W":
+      rover.direction === "S";
+   //console.log("The rover is facing South");
+      break; 
+    case "E":
+      rover.dirction === "N"; 
+   //console.log("The rover is facing North");
+      break; 
+    case "S":
+      rover.direction === "W"
+    //console.log("The rover is facing West");
+      break;
+  }
+  console.log("turnLeft was called!");
+}
+
+//turnLeft()
+
+//function when the rover turns right --> direction changes
+
+function turnRight(rover){
+  switch(rover.direction){
+    case "N":
+      rover.direction === "E";
+     //console.log("The rover is facing East");
+      break; 
+    case "E":
+      rover.direction === "S";
      //console.log("The rover is facing South");
-        break; 
-      case "E":
-        rover.dirction === "N"; 
-     //console.log("The rover is facing North");
-        break; 
-      case "S":
-        rover.direction === "W"
+      break; 
+    case "S":
+      rover.direction === "W";
       //console.log("The rover is facing West");
-        break;
-    }
-    console.log("turnLeft was called!");
+      break; 
+    case "W": 
+      rover.direction === "N"; 
+      //console.log("The rover is facing North");
+      break;
   }
-
-  //turnLeft()
+  console.log("turnRight was called!");
+  }
   
-  //function when the rover turns right --> direction changes
+//turnRight(rover);
 
-  function turnRight(rover){
-    switch(rover.direction){
-      case "N":
-        rover.direction === "E";
-       //console.log("The rover is facing East");
-        break; 
-      case "E":
-        rover.direction === "S";
-       //console.log("The rover is facing South");
-        break; 
-      case "S":
-        rover.direction === "W";
-        //console.log("The rover is facing West");
-        break; 
-      case "W": 
-        rover.direction === "N"; 
-        //console.log("The rover is facing North");
-        break;
-    }
-    console.log("turnRight was called!");
-    }
-    
-  //turnRight(rover);
 
-  
-  //function when the robot moves forward 
-  function moveForward(rover){
-    switch(rover.direction){
-      case "W": 
-        rover.x--;
-        console.log(`The rover has now parked on ${rover.x} and ${rover.y}`);
-        break;
-      case "E": 
-        rover.x++;
-        console.log(`The rover has now parked on ${rover.x} and ${rover.y}`);
-        break;
-      case "N": 
-        rover.y--;
-        console.log(`The rover has now parked on ${rover.x} and ${rover.y}`);
-        break;
+//function when the robot moves forward 
+function moveForward(rover){
+  switch(rover.direction){
+    case "W": 
+      rover.x--;
+      console.log(`The rover has now parked on ${rover.x} and ${rover.y}`);
+      break;
+    case "E": 
+      rover.x++;
+      console.log(`The rover has now parked on ${rover.x} and ${rover.y}`);
+      break;
+    case "N": 
+      rover.y++;
+      console.log(`The rover has now parked on ${rover.x} and ${rover.y}`);
+      break;
       case "S": 
-        rover.y++;
-        console.log(`The rover has now parked on ${rover.x} and ${rover.y}`);
-        break;
-    }
-      console.log("moveForward was called")
+      rover.y--;
+      console.log(`The rover has now parked on ${rover.x} and ${rover.y}`);
+      break;
   }
-  
-  moveForward(rover);
-  
-  function command(rover, order){
-    for (i=0; i<orders.length; i++){
-      let order = order[i];
-      switch (order){
-        case "f":
-          function moveForward(rover);
-          break; 
-        case "r":
-          function tunrRight(rover);
-          break; 
-        case "l":
-          function turnLeft(rover);
-          break;         
-      }
-    }
-  }
+    console.log("moveForward was called")
+}
 
-  command(rover, "rffrfflfrff");
-  
+moveForward(rover);
+
+var command = ["r","f","f","r","f","f","l","f","r","f","f"]
+
+function roverCommand(orders){
+  for (i=0; i<orders.length; i++){
+    console.log(i);
+    switch (orders[i]){
+      case "f":
+        moveForward(rover);
+        break; 
+      case "r":
+        turnRight(rover);
+        break; 
+      case "l":
+        turnLeft(rover);
+        break;         
+    }
+  }
+}
+
+console.log(roverCommand(command));
+
+roverCommand(command);
+
+//history of movements
+function travelLog(rover) {
+  rover.lastPosition = { x: rover.x, y: rover.y };
+  rover.travelLog.push(rover.lastPosition);
+}
+
+
+
+
+
